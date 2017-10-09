eeedb18881c43044c287c6c8393bb4f46c002dac
Dragon_Iron <maxl13@ukr.net>
2017-10-09 14:40:36 +0300

    Улучшение читабельности кода

--- fighter.py
+++ fighter.py
@@ -4,16 +4,19 @@ import random
 def hit(fighter):
     while True:
         h = input('Боец ' + str(fighter) + ', выберите действие: ')
+
         try:
             h = int(h)
         except:
             print('Вводите только цифры')
-            continue    
+            continue
+
         if h > 4 or h < 0:
             print('Вводите только цифры из списка')
             continue
         else:
             break
+
     return h
 
 
@@ -24,7 +27,7 @@ def fortune(fighter):
     return a + b 
 
 
-def isQuit(first, second):
+def is_quit(first, second):
     if first == 0 and second == 0:
         print('Боец 1: сдался')
         print('Боец 2: сдался')
@@ -44,7 +47,7 @@ def isQuit(first, second):
     return True    
 
 
-def isBlock(h_first, f_first, h_second, f_second):
+def is_block(h_first, f_first, h_second, f_second):
     if h_first == 4 and h_second == 4:
         print('Оба бойца поставили блок')
         return True
@@ -68,47 +71,47 @@ def isBlock(h_first, f_first, h_second, f_second):
     return False
 
 
-def hitStatus(fighter, hit, fortune):
-    if hit == 4:
+def hit_status(fighter, _hit, _fortune):
+    if _hit == 4:
         return 0
 
-    if fortune < 5:
+    if _fortune < 5:
         print('Боец ' + str(fighter) + ': неудачный удар')
         return 0
 
-    if 5 <= fortune <= 8:
-        if hit == 1:
+    if 5 <= _fortune <= 8:
+        if _hit == 1:
             print('Боец ' + str(fighter) + ': удар левой рукой')
             return 1
-        if hit == 2:
+        if _hit == 2:
             print('Боец ' + str(fighter) + ': удар правой рукой')
             return 2
-        if hit == 3:
+        if _hit == 3:
             print('Боец ' + str(fighter) + ': неудачный удар')
             return 0
 
-    if fortune > 8:
-        if hit == 1:
-            if fortune == 12:
+    if _fortune > 8:
+        if _hit == 1:
+            if _fortune == 12:
                 print('Боец ' + str(fighter) + ': критический удар левой рукой')
                 return 2
             print('Боец ' + str(fighter) + ': удар левой рукой')
             return 1
-        if hit == 2:
-            if fortune == 12:
+        if _hit == 2:
+            if _fortune == 12:
                 print('Боец ' + str(fighter) + ': критический удар правой рукой')
                 return 3
             print('Боец ' + str(fighter) + ': удар правой рукой')
             return 2
-        if hit == 3:
-            if fortune == 12:
+        if _hit == 3:
+            if _fortune == 12:
                 print('Боец ' + str(fighter) + ': критический удар ногой')
                 return 4
             print('Боец ' + str(fighter) + ': удар ногой')
             return 3
 
 
-def isKO(first, second):
+def is_ko(first, second):
     if first <= 0 and second <= 0:
         print('Оба бойца повержены')
         print('Ничья!')
@@ -153,21 +156,21 @@ def main():
                 hit2 = h
                 fortune2 = f
 
-        fight = isQuit(hit1, hit2)
+        fight = is_quit(hit1, hit2)
         if not fight:
             break
-        if isBlock(hit1, fortune1, hit2, fortune2):
+        if is_block(hit1, fortune1, hit2, fortune2):
             continue
 
         for n in range(1, 3):
             if n == 1:
-                damage = int(hitStatus(n, hit1, fortune1))
+                damage = int(hit_status(n, hit1, fortune1))
                 health2 -= damage
             else:
-                damage = int(hitStatus(n, hit2, fortune2))
+                damage = int(hit_status(n, hit2, fortune2))
                 health1 -= damage
 
-        out = isKO(health1, health2)
+        out = is_ko(health1, health2)
         if out:
             break
 
@@ -177,6 +180,7 @@ def main():
 
 while True:
     string = input('Чтобы начать игру нажмите Enter')
+
     if not string:
         main()
     else:
